591c425666c23ba604251c3ff6ddf1a0c3a28bf5
mujahidina <mujahidabdi5070@gmail.com>
2023-12-12 17:06:34 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+// Write your code here!
+document.getElementById("main").remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Mujahid is the champion"; // Replace YOUR-NAME with your name
+
+document.body.appendChild(newHeader);
